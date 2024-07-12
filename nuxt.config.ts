a872cc6633bc3583b3539be355c11bf96815dce0
weijingbbb <1047129443@qq.com>
2024-07-12 13:31:42 +0800

    安装tailwindcss，并安装相应模块

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  css: ['~/assets/css/main.css'],
+  modules: ["@nuxtjs/tailwindcss"]
 })
