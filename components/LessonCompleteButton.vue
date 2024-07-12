75577e7d93a21b910e5a026b3cc969885806f592
weijingbbb <1047129443@qq.com>
2024-07-12 16:04:04 +0800

    使用useState保存组件状态

---
+++ components/LessonCompleteButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <label class="px-4 py-2 font-bold text-white rounded cursor-pointer" :class="{
+        'bg-green-500': modelValue,
+        'bg-gray-500': !modelValue,
+    }">
+        <input type="checkbox" :value="modelValue" @input="() => $emit('update:modelValue', !modelValue)"
+            class="hidden" />
+        {{ modelValue ? 'Completed!' : 'Mark as complete' }}
+    </label>
+</template>
+
+<script setup>
+defineProps({
+    modelValue: {
+        type: Boolean,
+        default: false,
+    },
+});
+defineEmits(['update:modelValue']);
+</script>
+
+<style scoped>
+::selection {
+    display: none;
+}
+</style>
