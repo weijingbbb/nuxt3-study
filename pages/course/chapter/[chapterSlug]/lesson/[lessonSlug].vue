54f2a4b8653e247aa250d241d3cc073da8c2e2df
weijingbbb <1047129443@qq.com>
2024-07-12 15:44:05 +0800

    使用useHead更新标题

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -20,7 +20,6 @@
 <script setup>
 const course = useCourse();
 const route = useRoute();
-console.log(course);
 const chapter = computed(() => {
     return course.chapters.find(
         (chapter) => chapter.slug === route.params.chapterSlug
@@ -30,5 +29,12 @@ const lesson = computed(() => {
     return chapter.value.lessons.find(
         (lesson) => lesson.slug === route.params.lessonSlug
     );
+});
+
+const title = computed(() => {
+    return `${lesson.value.title} - ${course.title}`;
+});
+useHead({
+    title,
 });
 </script>
