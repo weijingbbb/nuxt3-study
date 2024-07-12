4a948d795525b5b9b9d6e6ee5a09ba5616c3cc50
weijingbbb <1047129443@qq.com>
2024-07-12 14:51:44 +0800

    更新样式

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,9 +1,19 @@
 <template>
-    <h2>Lesson</h2>
-    <p>This is a lesson</p>
-    <p>{{ chapter.title }}</p>
-    <p>{{ lesson.title }}</p>
-    <p>{{ lesson.text  }}</p>
+    <div>
+        <p class="mt-0 mb-1 font-bold uppercase text-slate-400">
+            Lesson {{ chapter.number }} - {{ lesson.number }}
+        </p>
+        <h2 class="my-0">{{ lesson.title }}</h2>
+        <div class="flex mt-2 mb-8 space-x-4">
+            <a v-if="lesson.sourceUrl" class="font-normal text-gray-500 text-md" :href="lesson.sourceUrl">
+                Download Source Code
+            </a>
+            <a v-if="lesson.downloadUrl" class="font-normal text-gray-500 text-md" :href="lesson.downloadUrl">
+                Download Video
+            </a>
+        </div>
+        <p>{{ lesson.text }}</p>
+    </div>
 </template>
 
 <script setup>
