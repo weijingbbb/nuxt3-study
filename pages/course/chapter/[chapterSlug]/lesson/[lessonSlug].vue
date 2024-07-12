c73fa97112e9daccfae776257f2ab094b198d13c
weijingbbb <1047129443@qq.com>
2024-07-12 14:46:42 +0800

    添加数据，匹配字段，路由为：/course/chapter/1-chapter-1/lesson/1-******

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,23 @@
+<template>
+    <h2>Lesson</h2>
+    <p>This is a lesson</p>
+    <p>{{ chapter.title }}</p>
+    <p>{{ lesson.title }}</p>
+    <p>{{ lesson.text  }}</p>
+</template>
+
+<script setup>
+const course = useCourse();
+const route = useRoute();
+console.log(course);
+const chapter = computed(() => {
+    return course.chapters.find(
+        (chapter) => chapter.slug === route.params.chapterSlug
+    );
+});
+const lesson = computed(() => {
+    return chapter.value.lessons.find(
+        (lesson) => lesson.slug === route.params.lessonSlug
+    );
+});
+</script>
