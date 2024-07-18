fcbf69e2b3eeac798ece85cd5a1bafe7f5d133cb
weijingbbb <1047129443@qq.com>
2024-07-19 01:07:36 +0800

    添加登出功能

--- components/UserCard.vue
+++ components/UserCard.vue
@@ -3,7 +3,7 @@
         <img class="w-12 h-12 border-2 border-blue-400 rounded-full" :src="profile" />
         <div class="text-right">
             <div class="font-medium">{{ name }}</div>
-            <button class="text-sm underline text-slate-500">
+            <button class="text-sm underline text-slate-500" @click="logout">
                 Log out
             </button>
         </div>
@@ -12,9 +12,28 @@
 
 <script setup lang="ts">
 const user = useSupabaseUser();
-console.log('user---', user?.value)
+const { auth } = useSupabaseClient();
+const logout = async () => {
+    const { error } = await auth.signOut();
+    if (error) {
+        console.error(error);
+        return;
+    }
+    // The Nuxt Supabase auth *should* be doing this
+    // for us, but it isn't for some reason.
+    try {
+        await $fetch('/api/_supabase/session', {
+            method: 'POST',
+            body: { event: 'SIGNED_OUT', session: null },
+        });
+        user.value = null;
+    } catch (e) {
+        console.error(error);
+    }
+    await navigateTo('/login');
+};
 const name = computed(
-    () => user?.value.user_metadata.full_name
+    () => user.value?.user_metadata.full_name
 );
 const profile = computed(
     () => user?.value.user_metadata.avatar_url
