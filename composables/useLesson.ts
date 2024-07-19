febe8c490e811b19c90e86fe7e1bc92f82691046
weijingbbb <1047129443@qq.com>
2024-07-19 19:34:02 +0800

    封装缓存hooks

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,40 +1,7 @@
-import { StorageSerializers } from '@vueuse/core';
 import { LessonWithPath } from '~~/types/course';
 
 export default async (
     chapterSlug: string,
     lessonSlug: string
-) => {
-    // Use sessionStorage to cache the lesson data
-    const url = `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`;
-    const lesson = useSessionStorage<LessonWithPath>(
-        url,
-        null,
-        {
-            // By passing null as default it can't automatically
-            // determine which serializer to use
-            serializer: StorageSerializers.object,
-        }
-    );
-
-    if (!lesson.value) {
-        const { data, error } = await useFetch<LessonWithPath>(
-            url
-        );
-
-        if (error.value) {
-            throw createError({
-                ...error.value,
-                statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
-            });
-        }
-
-        lesson.value = data.value;
-    } else {
-        console.log(
-            `Getting lesson ${lessonSlug} in chapter ${chapterSlug} from cache`
-        );
-    }
-
-    return lesson;
-};
+) => useFetchWithCache<LessonWithPath>(
+    `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`);
