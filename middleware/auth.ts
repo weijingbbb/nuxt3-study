8e5259343763543d2b3e02c5d958ce659cae5229
weijingbbb <1047129443@qq.com>
2024-07-19 19:21:15 +0800

    5-6，优先读取缓存里的数据，没有则进行请求新数据

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,8 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
 
     const user = useSupabaseUser();
-
-    console.log('--------',to.params.chapterSlug)
+    
     if (
         user.value ||
         to.params.chapterSlug === '1-chapter-1'
