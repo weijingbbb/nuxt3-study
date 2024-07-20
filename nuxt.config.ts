b2d9efe879908e26c885d29195fe4c0287abb0e7
weijingbbb <1047129443@qq.com>
2024-07-20 12:56:47 +0800

    5-11，修复prisma migrate dev 一直无响应的问题，主要修改 DATABASE_URL、DIRECT_URL，看官网找到connection配置，然后cv里面的链接到 DATABASE_URL、DIRECT_URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,8 @@ export default defineNuxtConfig({
 
 
   supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
     redirectOptions: {
       login: '/login',
       callback: '/',
